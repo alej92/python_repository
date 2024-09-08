ba06dced5c2b14d803fb0b90ee7f1dbb8fc56187
avpetrov27 <avpetrov27@yandex.ru>
2024-09-08 18:34:03 +0300

    lesson_03

---
+++ lesson_03/lesson_3_task_3.py
@@ -0,0 +1,9 @@
+from address import Address 
+from mailing import Mailing
+
+to_address = Address(126548, "Краснодар", "Комсомольская", 8, 103)
+from_address = Address(158725, "Москва", "Ленинский проспект", 8, 65)
+
+mailing = Mailing(to_address, from_address, 1000, 18452689623145)
+
+print(mailing)
